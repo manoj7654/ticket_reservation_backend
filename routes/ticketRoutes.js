eea660f2be4c2e26b20851bb120fdbbe5d86ab26
Manoj Kumar <manojsfstm5@gmail.com>
2023-07-07 23:28:51 +0530

    middleware added in book routes and get routes

--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -2,10 +2,11 @@ const express=require("express")
 const { book, searchTicket, getData } = require("../controller/ticket.controller")
 const tickeRouter=express.Router()
 
+const {authenticate}=require("../middleware/authentication")
 
-tickeRouter.post("/book/:movieId",book)
+tickeRouter.post("/book/:movieId",authenticate,book)
 tickeRouter.get("/get",searchTicket)
-tickeRouter.get("/getbooking",getData)
+tickeRouter.get("/getbooking",authenticate,getData)
 
 // tickeRouter.get("/allTheater",getTheater)
 // tickeRouter.get("/oneTheater/:theaterId",getOneTheater)
